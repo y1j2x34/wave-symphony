0ee1c37b78d0ede1dc1380924c2d755462a956f7
y1j2x34 <y1j2x34@qq.com>
2024-02-05 16:47:33 +0800

    chore: integrate tauri

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import { type Component } from 'solid-js';
+import { invoke } from '@tauri-apps/api';
 
 export function App() {
-    return <div class="text-4xl text-green-700 text-center py-20">Hello world!</div>
+    return <div>
+        <div class="text-4xl text-green-700 text-center py-20">Hello world!</div>
+        <button onClick={() => {
+            invoke('greet', { name: 'World' })
+            .then(response => {
+                console.log(response);
+            })
+        }}>Greet!</button>
+    </div>
 }
